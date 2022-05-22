c6956fdf3368f75e7a397991cae04e2ff65fe26b
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-05-22 16:57:26 -0600

    commit tree 1b877649ce5349203ef8604f562a2112d5f911ad

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+    attr_reader :finder
+    def initialize(finder)
+        @finder = finder
+    end
+    def match(new_word)
+        new_word.filter do |word|
+            if @finder.chars.sort == word.chars.sort
+                word
+            end
+        end
+    end
+end
+r1 = Anagram.new("listen")
+binding.pry
